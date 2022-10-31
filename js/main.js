7107e59389a873b8ed3f4c5f40b6cf1c93322dfe
John Phillips <john@simplyjohn.co.uk>
2022-10-31 22:48:50 +0000

    nav links animted

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initNavigation() {
   const mainNavLinks = gsap.utils.toArray(".main-nav a");
+  const mainNavLinksReversed = gsap.utils.toArray(".main-nav a").reverse();
+
   mainNavLinks.forEach((link) => {
+    // animate hover for each link using gsap utils
     link.addEventListener("mouseleave", (e) => {
       link.classList.add("animate-out");
 
@@ -10,6 +13,34 @@ function initNavigation() {
         link.classList.remove("animate-out");
       }, 300);
     });
+  });
+
+  // gsap to tween to animate nav items
+  function navAnimation(direction) {
+    // if positive scrollowing down is true
+    const scrollingDown = direction === 1;
+    const links = scrollingDown ? mainNavLinks : mainNavLinksReversed;
+    return gsap.to(links, {
+      duration: 1,
+      stagger: 0.5,
+      autoAlpha: () => (scrollingDown ? 0 : 1),
+      y: () => (scrollingDown ? 20 : 0),
+      ease: "power4.out",
+    });
+  }
+
+  ScrollTrigger.create({
+    start: 100,
+    toggleClass: {
+      targets: "body",
+      className: "has-scrolled",
+    },
+    // when scroll begins, run funtion
+
+    // deconstructing direction from onEnter event and passing it
+    onEnter: ({ direction }) => navAnimation(direction),
+    onLeaveBack: ({ direction }) => navAnimation(direction),
+    markers: true,
   });
 }
 
